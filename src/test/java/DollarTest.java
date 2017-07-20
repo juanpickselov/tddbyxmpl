2fd4340c6dfef1cf86c5430988aaa976122ac622
juanpickselov@sbcglobal.net <juanpickselov@sbcglobal.net>
2017-07-20 09:54:57 -0400

    renaming as I consider refactoring

--- src/test/java/DollarTest.java
+++ src/test/java/DollarTest.java
@@ -2,7 +2,7 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 
-public class MoneyExampleTest {
+public class DollarTest {
 
     @Test
     public void testMultiplication(){
